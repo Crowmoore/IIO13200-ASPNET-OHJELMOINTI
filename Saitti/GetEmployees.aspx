9623e730a26ea5e0d4593ce57f2607aa8b922ef6
Crowmoore <jokke.kortelainen@gmail.com>
2016-09-08 18:23:48 +0300

    Add xml databinding example

---
+++ Saitti/GetEmployees.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GetEmployees.aspx.cs" Inherits="GetEmployees" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>List of employees</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="controls">
+        <asp:Button ID="btnGetEmployees" runat="server" Text="Get employees" OnClick="btnGetEmployees_Click" />
+    </div>
+    <div id="display">
+        <asp:GridView ID="gridEmployeeData" runat="server"></asp:GridView>
+    </div>
+    <div id="footer">
+        <asp:Label ID="lblStatus" runat="server" Text="IMA FOOTER"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/GetEmployees.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration; //for web.config
+using System.Data; //ADO.NET classes
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GetEmployees : System.Web.UI.Page
+{
+    protected string xml;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        xml = ConfigurationManager.AppSettings["employeeXML"];
+        lblStatus.Text = "XML file named " + xml + " found";
+    }
+
+    protected void btnGetEmployees_Click(object sender, EventArgs e)
+    {
+        DataSet set = new DataSet();
+        DataTable table = new DataTable();
+        DataView view = new DataView();
+
+        set.ReadXml(Server.MapPath(xml));
+        table = set.Tables[0];
+        view = table.DefaultView;
+
+        gridEmployeeData.DataSource = view;
+        gridEmployeeData.DataBind();
+
+        lblStatus.Text = string.Format("Found {0} employees", table.Rows.Count);
+    }
+}
