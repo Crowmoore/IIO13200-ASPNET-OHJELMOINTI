a58eed920717cd026a6e237ef2c616d8ef6fb84b
Crowmoore <jokke.kortelainen@gmail.com>
2016-09-15 17:37:06 +0300

    Add database query example

---
+++ Saitti/Students.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Students.aspx.cs" Inherits="Students" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Students</h1>
+
+        <asp:Button ID="btnGet3Students" runat="server" Text="Get 3 students" OnClick="btnGet3Students_Click"/>
+        <asp:Button ID="btnGetAllStudents" runat="server" Text="Get all students" OnClick="btnGetAllStudents_Click"/>
+
+        <asp:GridView ID="gvData" runat="server" />
+    </div>
+    <div id="footer">
+        <asp:Label ID="lblMessages" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Students.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JAMK.ICT.Data;
+
+public partial class Students : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void btnGet3Students_Click(object sender, EventArgs e)
+    {
+        DataTable dt = DBPlacebo.Get3TestStudents();
+        gvData.DataSource = dt;
+        gvData.DataBind();
+    }
+    public void btnGetAllStudents_Click(object sender, EventArgs e)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["Students"].ConnectionString;
+        string message = "";
+        try
+        {
+            DataTable dt = DBPlacebo.GetAllStudentsFromSQLServer(connectionString, "oppilaat", out message);
+            gvData.DataSource = dt;
+            gvData.DataBind();
+            lblMessages.Text = message;
+        }
+        catch (Exception ex)
+        {
+            lblMessages.Text = ex.Message;
+        }
+    }
+}
