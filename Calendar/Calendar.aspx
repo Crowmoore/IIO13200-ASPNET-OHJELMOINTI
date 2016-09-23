36b98e6367fd9f58743dc5de0201b722eb8198d5
Crowmoore <jokke.kortelainen@gmail.com>
2016-09-23 13:59:40 +0300

    Started calendar exercise

---
+++ Calendar/Calendar.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Calendar.aspx.cs" Inherits="Calendar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblHeader" Font-Size="Large" Text="Current date here" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lblSelectedDate" Text="Selected date: " runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lblDateDifference" Text="Difference between current date and selected date: " runat="server"></asp:Label>
+        <br />
+        <asp:Button ID="btnPreviousYear" runat="server" Text="<< Year" OnClick="btnPreviousYear_Click"/>
+        <asp:Button ID="btnNextYear" runat="server" Text="Year >>" OnClick="btnNextYear_Click"/>
+        <asp:Calendar ID="calendar" runat="server" VisibleDate="<% #DateTime.Now %>" OnSelectionChanged="calendar_SelectionChanged"></asp:Calendar>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Calendar/Calendar.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Calendar : System.Web.UI.Page
+{
+    private int day;
+    private int month;
+    private int year;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        day = DateTime.Now.Day;
+        month = DateTime.Now.Month;
+        year = DateTime.Now.Year;
+        setCurrentDateToHeader();
+    }
+
+    private void setCurrentDateToHeader()
+    {
+        string date = DateTime.Now.ToString("dd.MM.yyyy");
+        lblHeader.Text = "Today is " + date;
+        calendar.VisibleDate = new DateTime(year, month, day);
+        calendar.SelectedDate = new DateTime(year, month, day);
+    }
+
+    protected void btnPreviousYear_Click(object sender, EventArgs e)
+    {
+        day = calendar.SelectedDate.Day;
+        month = calendar.SelectedDate.Month;
+        year = calendar.SelectedDate.Year;
+        calendar.VisibleDate = new DateTime(year - 1, month, day);
+        calendar.SelectedDate = new DateTime(year - 1, month, day);
+    }
+
+    protected void btnNextYear_Click(object sender, EventArgs e)
+    {
+        day = calendar.SelectedDate.Day;
+        month = calendar.SelectedDate.Month;
+        year = calendar.SelectedDate.Year;
+        calendar.VisibleDate = new DateTime(year + 1, month, day);
+        calendar.SelectedDate = new DateTime(year + 1, month, day);
+    }
+
+    protected void calendar_SelectionChanged(object sender, EventArgs e)
+    {
+        lblSelectedDate.Text = "Selected date: " + calendar.SelectedDate.ToString("dd.MM.yyyy");
+    }
+}
