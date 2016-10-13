34e1d5c9b2e3d9f41e46074da6b3053b4fd33a9b
Crowmoore <jokke.kortelainen@gmail.com>
2016-10-13 17:27:54 +0300

    Add xml examples

---
+++ Saitti/Feeds.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Feeds.aspx.cs" Inherits="Feeds" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <asp:Button ID="getFeedIL" runat="server" Text="Get RSS IL" OnClick="getFeedIL_Click"/>
+    <asp:Button ID="getFeedIS" runat="server" Text="Get RSS IS" OnClick="getFeedIS_Click"/>
+    <asp:XmlDataSource ID="dataSource" runat="server"></asp:XmlDataSource>
+    <div id="feed_div" runat="server">
+       
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ Saitti/Feeds.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Feeds : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void getFeedIL_Click(object sender, EventArgs e)
+    {
+        string url = @"http://www.iltalehti.fi/rss.xml";
+        dataSource.DataFile = url;
+        getFeeds();
+    }
+
+    protected void getFeedIS_Click(object sender, EventArgs e)
+    {
+        string url = @"http://www.iltasanomat.fi/rss/tuoreimmat.xml";
+        dataSource.DataFile = url;
+        getFeeds();
+    }
+
+    private void getFeeds()
+    {
+        try
+        {
+            XmlDocument document = new XmlDocument();
+            document = dataSource.GetXmlDocument();
+
+            XmlNode root = document.SelectSingleNode("/rss/channel");
+            string title = root["title"].InnerText;
+
+            feed_div.InnerHtml = string.Format("<h1>{0} {1}</h1>", title, DateTime.Now.ToShortDateString());
+
+            XmlNodeList titles = document.SelectNodes("/rss/channel/item");
+
+            foreach(XmlNode node in titles) {
+                string header = node["title"].InnerText;
+                string link = node["link"].InnerText;
+                string content = node["description"].InnerText;
+
+                feed_div.InnerHtml += string.Format("<h2>{0}</h2><br/><a href='{1}'>{1}</a><br/><p>{2}</p><br/>", header, link, content);
+            }
+
+        } catch (Exception ex)
+        {
+            feed_div.InnerHtml = ex.Message;
+        }
+    }
+}
